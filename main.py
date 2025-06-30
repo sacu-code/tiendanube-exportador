9cc300b1d9e585a46e5001128bcd7021f500534f
sacu-code <sacudigital@gmail.com>
2025-06-30 12:37:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
 from datetime import datetime
 
+# CONFIGURACIÓN
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+STORE_ID = 6250679
+
+# Conexión a Google Sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("reporte-ventas-Fibransur_2025").sheet1
+
+# Llamada a la API
+url = f"https://api.tiendanube.com/v1/{STORE_ID}/orders?per_page=200"
+headers = {
+    "Authentication": f"bearer {ACCESS_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+res = requests.get(url, headers=headers)
+if res.status_code != 200:
+    raise Exception(f"Error {res.status_code}: {res.text}")
+
+orders = res.json()
+
+# Limpia y carga la hoja
 sheet.clear()
 sheet.append_row(["Nro Orden", "Email", "Fecha", "Cliente", "DNI", "Medio de pago", "SKU", "Producto", "Precio producto", "Descuento", "Costo envío", "Total"])
 
