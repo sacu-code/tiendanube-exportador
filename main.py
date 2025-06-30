c65a96f872e8203e81b443d990a67ce7002b8f6b
sacu-code <sacudigital@gmail.com>
2025-06-30 12:38:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-import requests
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import os
 from datetime import datetime
 
-# CONFIGURACIÓN
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-STORE_ID = 6250679
-
-# Conexión a Google Sheets
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open("reporte-ventas-Fibransur_2025").sheet1
-
-# Llamada a la API
-url = f"https://api.tiendanube.com/v1/{STORE_ID}/orders?per_page=200"
-headers = {
-    "Authentication": f"bearer {ACCESS_TOKEN}",
-    "Content-Type": "application/json"
-}
-
-res = requests.get(url, headers=headers)
-if res.status_code != 200:
-    raise Exception(f"Error {res.status_code}: {res.text}")
-
-orders = res.json()
-
-# Limpia y carga la hoja
 sheet.clear()
 sheet.append_row(["Nro Orden", "Email", "Fecha", "Cliente", "DNI", "Medio de pago", "SKU", "Producto", "Precio producto", "Descuento", "Costo envío", "Total"])
 
@@ -37,7 +9,7 @@ for order in orders:
     email = order.get("contact_email", "")
     cliente = order.get("contact_name", "")
     dni = order.get("contact_identification", "")
-    medio_pago = order.get("payment_details", {}).get("method", "")
+    medio_pago = order.get("gateway_name", "")
     descuento = order.get("promotional_discount", {}).get("total_discount_amount", "")
     envio = order.get("shipping_cost_owner", "")
     total = order.get("total", "")
